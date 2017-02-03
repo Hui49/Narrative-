89c494e112e9049d87c03ecc406ae939ef760be4
Hui49 <gogogohui@gmail.com>
2017-02-02 21:43:54 -0500

    Kinect Part

---
+++ code/GettingStarted/Face.xaml.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+using Microsoft.Kinect.Toolkit;
+using Microsoft.Kinect.Toolkit.Controls;
+using System.IO.Ports;
+namespace GettingStarted
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Data;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using Microsoft.Kinect;
+    using Microsoft.Kinect.Toolkit;
+    using GettingStarted;
+    /// <summary>
+    /// Face.xaml 的交互逻辑
+    /// </summary>
+    /// 
+
+
+    public partial class Face : Window
+    {
+        private static readonly int Bgr32BytesPerPixel = (PixelFormats.Bgr32.BitsPerPixel + 7) / 8;
+        private readonly KinectSensorChooser sensorChooser = new KinectSensorChooser();
+        private WriteableBitmap colorImageWritableBitmap;
+        private byte[] colorImageData;
+        private ColorImageFormat currentColorImageFormat = ColorImageFormat.Undefined;
+
+        private float modifyValue = 3.0f;
+
+        //Instantiate the Kinect runtime. Required to initialize the device.
+
+        Skeleton[] skeletons;
+
+        //record the max/min distance of left/right hands
+        float maxLeft = 640f, minLeft = 0f;
+        float maxRight = 0f, minRight = 640f;
+        float prevLeft = 0f, prevRight = 0f;
+
+        //create your serial port
+        SerialPort myPort = new SerialPort();
+        //initialize your data
+        byte[] bData = new byte[1];
+
+
+        public Face()
+        {
+            InitializeComponent();
+
+            var faceTrackingViewerBinding = new Binding("Kinect") { Source = sensorChooser };
+            faceTrackingViewer.SetBinding(FaceTrackingViewer.KinectProperty, faceTrackingViewerBinding);
+
+            sensorChooser.KinectChanged += SensorChooserOnKinectChanged;
+
+            sensorChooser.Start();
+
+            bData[0] = 0;
+            ////// set your port //////
+            myPort.PortName = "COM3"; // depending on the port used
+            myPort.BaudRate = 9600;
+            myPort.Handshake = System.IO.Ports.Handshake.None;
+            myPort.Parity = Parity.None;
+            myPort.DataBits = 8;
+            myPort.StopBits = StopBits.One;
+            myPort.ReadTimeout = 50;
+            myPort.WriteTimeout = 50;
+            // Check here for Arduino program:  ////
+            // http://arduino.cc/en/Tutorial/Dimmer
+            // http://arduino.cc/en/Reference/Serial
+            ////////////////////////////////////////
+            if (!myPort.IsOpen)
+            {
+                try
+                {
+                    myPort.Open();
+                }
+                catch (Exception ex) { }
+            }
+        }
+        private void SensorChooserOnKinectChanged(object sender, KinectChangedEventArgs kinectChangedEventArgs)
+        {
+            KinectSensor oldSensor = kinectChangedEventArgs.OldSensor;
+            KinectSensor newSensor = kinectChangedEventArgs.NewSensor;
+
+            if (oldSensor != null)
+            {
+                oldSensor.AllFramesReady -= KinectSensorOnAllFramesReady;
+                oldSensor.ColorStream.Disable();
+                oldSensor.DepthStream.Disable();
+                oldSensor.DepthStream.Range = DepthRange.Default;
+                oldSensor.SkeletonStream.Disable();
+                oldSensor.SkeletonStream.EnableTrackingInNearRange = false;
+                oldSensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Default;
+            }
+
+            if (newSensor != null)
+            {
+                try
+                {
+                    newSensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+                    newSensor.DepthStream.Enable(DepthImageFormat.Resolution320x240Fps30);
+                    /*
+                    try
+                    {
+                        // This will throw on non Kinect For Windows devices.
+                        newSensor.DepthStream.Range = DepthRange.Near;
+                        newSensor.SkeletonStream.EnableTrackingInNearRange = true;
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        newSensor.DepthStream.Range = DepthRange.Default;
+                        newSensor.SkeletonStream.EnableTrackingInNearRange = false;
+                    }
+                    */
+                    newSensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Seated;
+                    newSensor.SkeletonStream.Enable();
+                    newSensor.AllFramesReady += KinectSensorOnAllFramesReady;
+                }
+                catch (InvalidOperationException)
+                {
+                    // This exception can be thrown when we are trying to
+                    // enable streams on a device that has gone away.  This
+                    // can occur, say, in app shutdown scenarios when the sensor
+                    // goes away between the time it changed status and the
+                    // time we get the sensor changed notification.
+                    //
+                    // Behavior here is to just eat the exception and assume
+                    // another notification will come along if a sensor
+                    // comes back.
+                }
+            }
+        }
+
+        private void WindowClosed(object sender, EventArgs e)
+        {
+            sensorChooser.Stop();
+            faceTrackingViewer.Dispose();
+        }
+
+        private void KinectSensorOnAllFramesReady(object sender, AllFramesReadyEventArgs allFramesReadyEventArgs)
+        {
+            using (var colorImageFrame = allFramesReadyEventArgs.OpenColorImageFrame())
+            {
+                if (colorImageFrame == null)
+                {
+                    return;
+                }
+
+                // Make a copy of the color frame for displaying.
+                var haveNewFormat = this.currentColorImageFormat != colorImageFrame.Format;
+                if (haveNewFormat)
+                {
+                    this.currentColorImageFormat = colorImageFrame.Format;
+                    this.colorImageData = new byte[colorImageFrame.PixelDataLength];
+                    this.colorImageWritableBitmap = new WriteableBitmap(
+                        colorImageFrame.Width, colorImageFrame.Height, 96, 96, PixelFormats.Bgr32, null);
+                    ColorImage.Source = this.colorImageWritableBitmap;
+                }
+
+                colorImageFrame.CopyPixelDataTo(this.colorImageData);
+                this.colorImageWritableBitmap.WritePixels(
+                    new Int32Rect(0, 0, colorImageFrame.Width, colorImageFrame.Height),
+                    this.colorImageData,
+                    colorImageFrame.Width * Bgr32BytesPerPixel,
+                    0);
+
+                /*
+                double length = Point.Subtract(faceTrackingViewer.ppLeft, faceTrackingViewer.ppRight).Length;
+                tB.Text = length.ToString();
+                if (length > 19)
+                {
+                    elp.Fill = Brushes.Red;
+                }
+                else
+                {
+                    elp.Fill = Brushes.Green;
+                }
+                */
+
+                double mouthWidth = faceTrackingViewer.mouthWidth;
+                double noseWidth = faceTrackingViewer.noseWidth;
+
+                double threshold = noseWidth * modifyValue;
+
+
+
+                if (mouthWidth > threshold)
+                {
+                    elp.Fill = Brushes.Red;
+                    bData[0] = 3;
+                }
+                else
+                {
+                    elp.Fill = Brushes.Green;
+                    bData[0] = 3;
+                }
+            }
+        }
+    }
+}
+
---
+++ code/GettingStarted/MainWindow.xaml.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+using Microsoft.Kinect.Toolkit;
+using Microsoft.Kinect.Toolkit.Controls;
+using System.IO.Ports;
+
+namespace GettingStarted
+{
+   
+    public partial class MainWindow : Window
+    {
+        private KinectSensorChooser sensorChooser;
+
+        //Instantiate the Kinect runtime. Required to initialize the device.
+        
+        Skeleton[] skeletons;
+
+        //record the max/min distance of left/right hands
+        float maxLeft = 640f, minLeft = 0f;
+        float maxRight = 0f, minRight = 640f;
+        float prevLeft = 0f, prevRight = 0f;
+
+        //create your serial port
+        SerialPort myPort = new SerialPort();
+        //initialize your data
+        byte[] bData = new byte[1];
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+        
+
+            Loaded += OnLoaded;
+
+            
+           
+
+         
+
+            bData[0] = 0;
+            ////// set your port //////
+            myPort.PortName = "COM3"; // depending on the port used
+            myPort.BaudRate = 9600;
+            myPort.Handshake = System.IO.Ports.Handshake.None;
+            myPort.Parity = Parity.None;
+            myPort.DataBits = 8;
+            myPort.StopBits = StopBits.One;
+            myPort.ReadTimeout = 50;
+            myPort.WriteTimeout = 50;
+            // Check here for Arduino program:  ////
+            // http://arduino.cc/en/Tutorial/Dimmer
+            // http://arduino.cc/en/Reference/Serial
+            ////////////////////////////////////////
+            if (!myPort.IsOpen)
+            {
+                try
+                {
+                    myPort.Open();
+                }
+                catch (Exception ex) { }
+            }
+
+        }
+
+    
+
+        
+
+        void readData(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            using (SkeletonFrame mySFrame = e.OpenSkeletonFrame())
+            {
+                if (mySFrame != null)
+                {
+                    skeletons = new Skeleton[mySFrame.SkeletonArrayLength];
+                    mySFrame.CopySkeletonDataTo(skeletons);
+                    Skeleton currSkeleton = (from s in skeletons
+                                             where s.TrackingState == SkeletonTrackingState.Tracked
+                                             select s).FirstOrDefault();
+
+                    if (currSkeleton != null)
+                    {
+                        CalcDirection(currSkeleton);
+                    }
+                    
+                }
+            }
+            
+        }
+
+          private void CalcDirection(Skeleton mySkeleton)
+        {
+            Joint lJoint = mySkeleton.Joints[JointType.HandLeft];
+            Joint rJoint = mySkeleton.Joints[JointType.HandRight];
+            Joint flJoint = mySkeleton.Joints[JointType.KneeLeft];
+            Joint frJoint = mySkeleton.Joints[JointType.KneeRight];
+            Joint hJoint = mySkeleton.Joints[JointType.Head];
+
+            Microsoft.Kinect.SkeletonPoint lVector = new Microsoft.Kinect.SkeletonPoint();
+            lVector.X = ScaleVector(640, lJoint.Position.X);
+            lVector.Y = ScaleVector(480, -lJoint.Position.Y);
+            lVector.Z = lJoint.Position.Z;
+
+            Microsoft.Kinect.SkeletonPoint rVector = new Microsoft.Kinect.SkeletonPoint();
+            rVector.X = ScaleVector(640, rJoint.Position.X);
+            rVector.Y = ScaleVector(480, -rJoint.Position.Y);
+            rVector.Z = rJoint.Position.Z;
+
+            lJoint.TrackingState = JointTrackingState.Tracked;
+            if (lVector.X > prevLeft)
+            {
+                maxLeft = lVector.X;
+            }
+            if (lVector.X < prevLeft)
+            {
+                minLeft = lVector.X;
+                if (maxLeft > minLeft)
+                {
+                    bData[0] = 1;
+                    
+                  
+                       
+                        System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"C:\Users\lenovo\Desktop\brazil music.wav");
+                        player.Play();
+                        myPort.Write(bData, 0, 1);//output the siginal
+                       
+                       
+                }
+            }
+            prevLeft = lVector.X;
+
+            rJoint.TrackingState = JointTrackingState.Tracked;
+            if (rVector.X < prevRight)
+            {
+                minRight = rVector.X;
+            }
+            if (rVector.X > prevRight)
+            {
+                maxRight = rVector.X;
+                if (maxRight > minRight)
+                {
+                    bData[0] = 1;
+                   
+                 
+                        
+                        myPort.Write(bData, 0, 1);
+                        System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"C:\Users\lenovo\Desktop\brazil music.wav");
+                        player.Play();
+                      
+
+                }
+            }
+            prevRight = rVector.X;
+
+            if (flJoint.Position.Y > frJoint.Position.Y+10)
+            {
+                bData[0] = 2;
+                
+                try
+                {
+                    myPort.Write(bData, 0, 1);//output the siginal
+                }
+                catch (Exception ex) { }
+            }
+            if (flJoint.Position.Y +10< frJoint.Position.Y)
+            {
+                bData[0] = 2;
+                
+                try
+                {
+                    myPort.Write(bData, 0, 1);//output the siginal
+                }
+                catch (Exception ex) { }
+            }
+            float rjointy = rJoint.Position.Y;
+            float hjointy = hJoint.Position.Y;
+            if (rjointy > hjointy)
+            {
+                bData[0] = 2;
+
+
+                try
+                {
+                    myPort.Write(bData, 0, 1);//output the siginal
+                }
+                catch (Exception ex) {
+                    hjointy = -1000;
+                }
+              
+
+            }
+            
+             float ljointy = lJoint.Position.Y;
+
+             if (ljointy > hjointy)  
+             {
+                 bData[0] = 3;
+
+
+                 myPort.Write(bData, 0, 1);//output the siginal
+                 hjointy = -1000;
+
+
+             }
+
+           
+           
+        }
+
+        //vector scaling was imported from the Coding4Fun Kinect Toolkit http://c4fkinect.codeplex.com/
+        private float ScaleVector(int length, float position)
+        {
+            float value = (((float)length / 2f) * position) + (length / 2);
+            if (value > length)
+                return (float)length;
+            if (value < 0f)
+                return 0f;
+            return value;
+        }
+
+        
+
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+
+            this.sensorChooser = new KinectSensorChooser();
+            this.sensorChooser.KinectChanged += SensorChooserOnKinectChanged;
+            this.sensorChooserUi.KinectSensorChooser = this.sensorChooser;
+            this.sensorChooser.Start();
+
+            //fill scroll content
+           
+        }
+
+       
+        
+
+        private void SensorChooserOnKinectChanged(object sender, KinectChangedEventArgs args)
+        {
+            bool error = false;
+            if (args.OldSensor != null)
+            {
+                try
+                {
+                    args.OldSensor.DepthStream.Range = DepthRange.Default;
+                    args.OldSensor.SkeletonStream.EnableTrackingInNearRange = false;
+                    args.OldSensor.DepthStream.Disable();
+                    args.OldSensor.SkeletonStream.Disable();
+                }
+                catch (InvalidOperationException)
+                {
+                    // KinectSensor might enter an invalid state while enabling/disabling streams or stream features.
+                    // E.g.: sensor might be abruptly unplugged.
+                    error = true;
+                }
+            }
+
+            if (args.NewSensor != null)
+            {
+                try
+                {
+                    args.NewSensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+                    
+                    args.NewSensor.SkeletonFrameReady += readData;
+
+                    args.NewSensor.Start();
+                    args.NewSensor.SkeletonStream.Enable(); // Enable skeletal tracking
+                    try
+                    {
+                        args.NewSensor.DepthStream.Range = DepthRange.Near;
+                        args.NewSensor.SkeletonStream.EnableTrackingInNearRange = true;
+                        args.NewSensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Seated;
+                        args.NewSensor.SkeletonFrameReady += readData;
+
+                        args.NewSensor.Start();
+                        args.NewSensor.SkeletonStream.Enable(); // Enable skeletal tracking
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        // Non Kinect for Windows devices do not support Near mode, so reset back to default mode.
+                        args.NewSensor.DepthStream.Range = DepthRange.Default;
+                        args.NewSensor.SkeletonStream.EnableTrackingInNearRange = false;
+                        error = true;
+                    }
+                }
+                catch (InvalidOperationException)
+                {
+                    error = true;
+                    // KinectSensor might enter an invalid state while enabling/disabling streams or stream features.
+                    // E.g.: sensor might be abruptly unplugged.
+                }
+            }
+
+            if (!error)
+                kinectRegion.KinectSensor = args.NewSensor;
+        }
+
+       
+
+        private void ButtonOnClick1(object sender, RoutedEventArgs e)
+        {
+            var brazil = new brazil();
+            this.sensorChooser.Stop();
+            this.Close();
+            brazil.ShowDialog();
+           
+        }
+
+        private void ButtonOnClick2(object sender, RoutedEventArgs e)
+        {
+            var italy = new italy();
+            this.sensorChooser.Stop();
+            this.Close();
+            italy.ShowDialog();
+        }
+
+        private void ButtonOnClick3(object sender, RoutedEventArgs e)
+        {
+            var spain = new spain();
+            this.sensorChooser.Stop();
+            this.Close();
+            spain.ShowDialog();
+        }
+
+        private void ButtonOnClick4(object sender, RoutedEventArgs e)
+        {
+            var kean = new kean();
+            this.sensorChooser.Stop();
+            this.Close();
+            kean.ShowDialog();
+        }
+
+        private void ButtonOnClick5(object sender, RoutedEventArgs e)
+        {
+            var Face = new Face();
+            this.sensorChooser.Stop();
+            this.Close();
+            Face.ShowDialog();
+        }
+
+                
+    }
+}
+//http://dotneteers.net/blogs/vbandi/archive/2013/03/25/kinect-interactions-with-wpf-part-i-getting-started.aspx
